554ca6a8952db657094460ed827d8a3cd8142015
parastoo78 <pari.hoo1378@gmail.com>
2025-03-15 21:24:03 +0330

    Update smooth-scroll.js

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,14 +1,12 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.css">
-<script src="https://cdn.jsdelivr.net/npm/locomotive-scroll@4.1.1/dist/locomotive-scroll.min.js"></script>
-<script>
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".locomotive-scroll"),
-  smooth: true,
-  multiplier: 1.0,
-}); 
+<script charset="utf-8">  
+ var isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
+ if (!isMobile) {
+      luxy.init({
+        wrapper: '#luxy',
+        wrapperSpeed: 0.065,
+       
+    });
+ }
+ </script>
 
-// Wait 2 seconds then calculate the new page height
-setTimeout(() => {  
-	locoScroll.update();
-}, 2000);
-</script>
+
