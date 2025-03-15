8d475629617789a441f767a453ece2cff3abc0ab
parastoo78 <pari.hoo1378@gmail.com>
2025-03-15 11:56:33 +0330

    Update smooth-scroll.js

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,9 +1,14 @@
-// این کد باید در داخل یک فایل JS قرار بگیره یا توی Webflow داخل Custom Code قرار بدی.
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.css">
+<script src="https://cdn.jsdelivr.net/npm/locomotive-scroll@4.1.1/dist/locomotive-scroll.min.js"></script>
+<script>
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".locomotive-scroll"),
+  smooth: true,
+  multiplier: 1.0,
+}); 
+
+// Wait 2 seconds then calculate the new page height
+setTimeout(() => {  
+	locoScroll.update();
+}, 2000);
+</script>
