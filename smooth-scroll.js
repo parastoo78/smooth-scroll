05d531063cd1054aba8f58a7894d369e0c039f73
parastoo78 <pari.hoo1378@gmail.com>
2025-03-16 08:22:44 +0330

    Update smooth-scroll.js

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,12 +1,9 @@
-<script charset="utf-8">  
- var isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
- if (!isMobile) {
-      luxy.init({
-        wrapper: '#luxy',
-        wrapperSpeed: 0.065,
-       
-    });
- }
- </script>
-
-
+document.addEventListener("DOMContentLoaded", function () {
+    var isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
+    if (!isMobile) {
+        luxy.init({
+            wrapper: '#luxy',
+            wrapperSpeed: 0.065,
+        });
+    }
+});
